54da08618bcb409298a073a60a386e995a932f7c
Mitchell <mramosna@udel.edu>
2024-04-12 03:11:53 -0400

    added scoring

--- src/objects/swap.ts
+++ src/objects/swap.ts
@@ -1,8 +1,10 @@
 import Phaser from "phaser";
+import StageOne from "../scenes/stageOne";
 
 export default class Swap {
     private scene: Phaser.Scene;
     private selected_tile: Phaser.GameObjects.Sprite | null = null;
+    private border: Phaser.GameObjects.Graphics | null = null;
 
     constructor(scene: Phaser.Scene) {
         this.scene = scene;
@@ -15,23 +17,78 @@ export default class Swap {
                 tile.on("pointerdown", () => {
                     if (!this.selected_tile) {
                         this.selected_tile = tile;
+                        this.highlightTile(tile);
                     } else {
                         this.swapTiles(this.selected_tile, tile);
                         this.selected_tile = null;
+                        this.removeHighlight();
+                        this.rowCheck(group);
                     }
                 });
             }
         );
     }
 
+    private highlightTile(tile: Phaser.GameObjects.Sprite) {
+        this.border = this.scene.add.graphics();
+        this.border.lineStyle(10, 0xffef79);
+        this.border.strokeRect(
+            tile.x - tile.width / 2,
+            tile.y - tile.height / 2,
+            tile.width,
+            tile.height
+        );
+    }
+
+    private removeHighlight() {
+        if (this.border) {
+            this.border.destroy();
+            this.border = null;
+        }
+    }
+
     private swapTiles(
         tile1: Phaser.GameObjects.Sprite,
         tile2: Phaser.GameObjects.Sprite
     ) {
+        // Swap positions
         const tempX = tile1.x;
         const tempY = tile1.y;
-
         tile1.setPosition(tile2.x, tile2.y);
         tile2.setPosition(tempX, tempY);
+
+        // Swap names
+        const tempName = tile1.name;
+        tile1.name = tile2.name;
+        tile2.name = tempName;
+    }
+
+    private rowCheck(group: Phaser.GameObjects.Group) {
+        const children = group.getChildren() as Phaser.GameObjects.Sprite[];
+        const columns = 3;
+        const rows = 3;
+
+        for (let row = 0; row < rows; row++) {
+            const rowTiles: Phaser.GameObjects.Sprite[] = [];
+            for (let col = 0; col < columns; col++) {
+                const index = col + row * columns;
+                const tile = children[index];
+                rowTiles.push(tile);
+            }
+            this.doSomethingWithTile(rowTiles);
+        }
+    }
+
+    private doSomethingWithTile(rowTiles: Phaser.GameObjects.Sprite[]) {
+        let row1 = rowTiles[0].name + rowTiles[1].name + rowTiles[2].name;
+        console.log(row1);
+
+        if (
+            row1.includes("tort") ||
+            row1.includes("torf") ||
+            row1.includes("tandt")
+        ) {
+            StageOne.score += 500;
+        }
     }
 }
--- src/scenes/stageOne.ts
+++ src/scenes/stageOne.ts
@@ -4,6 +4,8 @@ import Swap from "../objects/swap";
 export default class StageOne extends Phaser.Scene {
     private boardGroup: Phaser.GameObjects.Group;
     private swap: Swap;
+    private scoreText: Phaser.GameObjects.Text;
+    static score: number = 0;
 
     constructor() {
         super({ key: "StageOne" });
@@ -17,7 +19,7 @@ export default class StageOne extends Phaser.Scene {
     }
 
     create() {
-        this.cameras.main.setBackgroundColor("#61F1FF");
+        this.cameras.main.setBackgroundColor("#C9E5F3");
         // creates a 3x3 grid (450/150 = 3, hence 3x3)
         const grid = this.add.grid(
             this.cameras.main.width / 2,
@@ -28,30 +30,50 @@ export default class StageOne extends Phaser.Scene {
             150,
             0x909090
         );
-        const message = "List: " + grid.cellHeight + ", " + grid.cellWidth;
-        this.add
-            .text(this.cameras.main.width - 15, 15, message, {
-                color: "#000000",
-                fontSize: "24px",
-            })
+        this.scoreText = this.add
+            .text(
+                this.cameras.main.width - 15,
+                15,
+                "Score: " + StageOne.score,
+                {
+                    color: "#000000",
+                    fontSize: "24px",
+                }
+            )
             .setOrigin(1, -1);
-
-        const group = this.add.group({
+        /*const group = this.add.group({
             key: "tiles",
             frame: [0, 2, 3],
             frameQuantity: 3,
             randomKey: true,
-        });
+        });*/
+
+        // add tiles with unique keys corresponding to their value
+        const group = this.add.group();
+        group.add(this.add.sprite(0, 0, "tiles", 0).setName("and"));
+        for (let i = 0; i < 2; i++) {
+            group.add(this.add.sprite(0, 0, "tiles", 2).setName("f"));
+            group.add(this.add.sprite(0, 0, "tiles", 1).setName("or"));
+        }
+        for (let i = 0; i < 4; i++) {
+            group.add(this.add.sprite(0, 0, "tiles", 3).setName("t"));
+        }
+
+        Phaser.Utils.Array.Shuffle(group.getChildren());
 
         Phaser.Actions.GridAlign(group.getChildren(), {
             width: 3,
             height: 3,
             cellWidth: 150,
             cellHeight: 150,
-            x: 410,
-            y: 130,
+            x: 416,
+            y: 135,
         });
         this.swap = new Swap(this);
         this.swap.tileSetup(group);
+    }
+
+    update() {
+        this.scoreText.setText("Score: " + StageOne.score);
     }
 }

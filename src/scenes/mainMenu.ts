11d1636577540800e0e21686707e7d45ca57b4ae
Emma <emmreese@udel.edu>
2024-04-30 17:10:05 -0400

    Made buttons

--- src/scenes/mainMenu.ts
+++ src/scenes/mainMenu.ts
@@ -2,6 +2,8 @@ import Phaser from 'phaser';
 
 export default class MainMenuScene extends Phaser.Scene {
     private cursors!: Phaser.Types.Input.Keyboard.CursorKeys;
+    private tutorialButton!: Phaser.GameObjects.Text;
+    private gameButton!: Phaser.GameObjects.Text;
 
     constructor() {
         super('main-menu');
@@ -16,7 +18,14 @@ export default class MainMenuScene extends Phaser.Scene {
     }
 
     create() {
-        
+        this.tutorialButton = this.add.text(100, 100, 'Tutorial', { fill: '#0f0' }).setInteractive();
+        this.tutorialButton.on('pointerdown', () => {
+        });
+ 
+        this.gameButton = this.add.text(100, 150, 'Play', { fill: '#f00' }).setInteractive();
+        this.gameButton.on('pointerdown', () => {
+        });
+    
     }
 
 

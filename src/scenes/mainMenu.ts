463e188ae711be7f2b7b0efde81e332997aee5dd
Emma <emmreese@udel.edu>
2024-05-02 16:27:30 -0400

    fixed it

--- src/scenes/mainMenu.ts
+++ src/scenes/mainMenu.ts
@@ -1,40 +1,37 @@
-import Phaser from "phaser";
+import Phaser from 'phaser';
 
 export default class MainMenuScene extends Phaser.Scene {
-    private cursors: Phaser.Types.Input.Keyboard.CursorKeys;
+    private cursors!: Phaser.Types.Input.Keyboard.CursorKeys;
     private tutorialButton!: Phaser.GameObjects.Text;
     private gameButton!: Phaser.GameObjects.Text;
 
     constructor() {
-        super("main-menu");
+        super('main-menu');
     }
 
     init() {
-        const cursorkeys = this.input.keyboard?.createCursorKeys();
-        if (cursorkeys) {
-            this.cursors = cursorkeys;
-        } else {
-            console.error("error");
-        }
+        this.cursors = this.input.keyboard.createCursorKeys();
     }
 
-    preload() {}
+    preload() {
+        
+    }
 
     create() {
-        this.tutorialButton = this.add
-            .text(100, 100, "Tutorial", { color: "#0f0" })
-            .setInteractive();
-        this.tutorialButton.on("pointerdown", () => {
-            this.scene.start("tutorialScene");
+        this.tutorialButton = this.add.text(100, 100, 'Tutorial', { fill: '#0f0' }).setInteractive();
+        this.tutorialButton.on('pointerdown', () => {
+            this.scene.start('tutorialScene');
         });
-
-        this.gameButton = this.add
-            .text(100, 150, "Play", { color: "#f00" })
-            .setInteractive();
-        this.gameButton.on("pointerdown", () => {
-            this.scene.start("stageOne");
+ 
+        this.gameButton = this.add.text(100, 150, 'Play', { fill: '#f00' }).setInteractive();
+        this.gameButton.on('pointerdown', () => {
+            this.scene.start('stageOne');
         });
+    
     }
 
-    update() {}
+
+    update() {
+        
+    }
 }
